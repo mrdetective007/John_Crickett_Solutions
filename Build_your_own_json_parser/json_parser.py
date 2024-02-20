c2b914a99ac6d5a874994384cd929d6abb700918
Jeet Shah <97144131+mrdetective007@users.noreply.github.com>
2024-02-20 23:13:52 +0530

    Add files via upload

---
+++ Build_your_own_json_parser/json_parser.py
@@ -0,0 +1,21 @@
+import json
+import sys
+
+path = "test\\test\\" + sys.argv[1]
+file_ptr = open(path,)
+
+
+try:
+    data = json.load(file_ptr)
+    if type(data) is dict or type(data) is list:
+        print(data)
+        print("Exiting the code with exit code 0")
+    else:
+        print("Invalid Syntax")
+        print("Exiting with the exit code 1")
+except (json.JSONDecodeError, ValueError, KeyError) as e:
+    print(e)
+    print("Exiting the code with exit code 1")
+
+
+file_ptr.close()
