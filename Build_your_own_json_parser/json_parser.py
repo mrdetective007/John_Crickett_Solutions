c6ce0a35efa8d425a7384ded8f68d9d097af357c
Jeet Shah <97144131+mrdetective007@users.noreply.github.com>
2024-02-22 20:48:31 +0530

    Update json_parser.py

--- Build_your_own_json_parser/json_parser.py
+++ Build_your_own_json_parser/json_parser.py
@@ -10,12 +10,15 @@ try:
     if type(data) is dict or type(data) is list:
         print(data)
         print("Exiting the code with exit code 0")
+        sys.exit(0)
     else:
         print("Invalid Syntax")
         print("Exiting with the exit code 1")
+        sys.exit(1)
 except (json.JSONDecodeError, ValueError, KeyError) as e:
     print(e)
     print("Exiting the code with exit code 1")
+    sys.exit(1)
 
 
 file_ptr.close()
